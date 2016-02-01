e43d4504d4825469393678c08638b77063c2ba31
mbattur <mbattur@yahoo.com>
2016-02-01 00:26:43 +0000

    a 32 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,14 @@
 end
 posts = Post.all
 
+20.times do
+  Advertisement.create!(
+    title: RandomData.random_sentence,
+    copy: RandomData.random_paragraph
+    )
+end
+advertisements = Advertisement.all
+
 100.times do
   Comment.create!(
     post: posts.sample,
@@ -16,3 +24,4 @@ end
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Advertisement.count} ads created"
---
+++ spec/models/advertisement_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Advertisement, type: :model do
+  let(:advertisement) { Advertisement.create!(title: "New Ad Title", copy: "New Ad Copy") }
+  
+  describe "attributes" do
+    it "responds to title" do
+      expect(advertisement).to respond_to(:title)
+    end
+    
+    it "responds to copy" do
+      expect(advertisement).to respond_to(:copy)
+    end
+    
+    it "respond to price" do
+      expect(advertisement).to respond_to(:price)
+    end
+  end
+end
