01a0070069bb0fd48bb5f02001a35e0d5b801de5
mbattur <mbattur@yahoo.com>
2016-02-03 16:03:53 +0000

    fixing stuff

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 20160118191056) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-<<<<<<< HEAD
     t.integer  "topic_id"
   end
 
@@ -39,8 +38,5 @@ ActiveRecord::Schema.define(version: 20160118191056) do
     t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
-=======
->>>>>>> parent of 3c85c92... Merge pull request #1 from mbattur/check-34
   end
-
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,12 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
 
-<<<<<<< HEAD
  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
-=======
- let(:post) { Post.create!(title: "New Post Title", body: "New Post body") }
->>>>>>> parent of 3c85c92... Merge pull request #1 from mbattur/check-34
  let(:comment) { Comment.create!(body: 'Comment Body', post: post) }
  
  describe "attributes" do
