3041949d2e16ef603978b4c447b01d5dcb246ae8
mbattur <mbattur@yahoo.com>
2016-02-12 00:04:47 +0000

    continue check 40

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
   has_many :posts
+  
   before_save { self.email = email.downcase }
+  before_save { self.role ||= :member }
+  
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
@@ -13,4 +17,6 @@ class User < ActiveRecord::Base
             format: { with: EMAIL_REGEX }
             
   has_secure_password
+  
+  enum role: [:member, :admin]
 end
